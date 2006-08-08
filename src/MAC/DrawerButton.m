7b46d182ddfe6930f8d48b1076a372fe71c00a18
neil <neil>
2006-08-08 19:15:35 +0000

    Remove last AGRegex dependency, then remove AGRegex

--- src/MAC/DrawerButton.m
+++ src/MAC/DrawerButton.m
@@ -1,6 +1,5 @@
 // This file is hereby placed in the Public Domain -- Neil Stevens
 
-#import "AGRegex.h"
 #import "DrawerButton.h"
 
 @implementation DrawerButton
@@ -19,9 +18,12 @@
 {
 	int state = [drawer state];
 	bool opening = state == NSDrawerOpenState | state == NSDrawerOpeningState;
-	NSString *newText = opening ? @"Hide" : @"Show";
-	AGRegex *re = [AGRegex regexWithPattern:@"^(Show|Hide)"];
-	[self setTitle:[re replaceWithString:newText inString:[self title]]];
+	NSString *newText = opening ? @"Hide " : @"Show ";
+	if([[self title] hasPrefix:@"Hide "] || [[self title] hasPrefix:@"Show "])
+	{
+		[self setTitle:[newText stringByAppendingString:
+		      [[self title] substringFromIndex:5]]];
+	}
 }
 
 @end
